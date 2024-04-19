fcf38481483304dc6d440e0c812c94346c29f150
Sajani <sajju2315@gmail.com>
2024-04-18 21:00:40 -0400

    Added more tests for multiEmptyLine

--- tests/format/typescript/multiEmptyLine/multiEmptyLine.ts
+++ tests/format/typescript/multiEmptyLine/multiEmptyLine.ts
@@ -11,3 +11,45 @@ function add(x, y) {
 
 
   }
+
+  function addTwo(x, y) {
+
+
+    //comment
+    
+
+    return x + y;
+
+   
+
+
+
+  }
+
+  function addThree(x, y) {
+
+
+    
+    
+
+    return x + y;
+
+    //comment
+
+
+
+  }
+
+  function addFour(x, y) {
+
+
+    
+    
+
+    return x + y;
+
+ 
+
+
+
+  }
