20ae50e678566f43d74cfeada8bc776fa59f219f
Sajani <sajju2315@gmail.com>
2024-04-18 23:04:25 -0400

    Revised setting name in jsfmt.spec.js

--- tests/format/typescript/multiEmptyLine/jsfmt.spec.js
+++ tests/format/typescript/multiEmptyLine/jsfmt.spec.js
@@ -1,2 +1,2 @@
-run_spec(__dirname, ["babel", "flow", "typescript"], {retainBlankLines: false});
-run_spec(__dirname, ["babel", "flow", "typescript"], {retainBlankLines: true});
+run_spec(__dirname, ["babel", "flow", "typescript"], {multiEmptyLine: false});
+run_spec(__dirname, ["babel", "flow", "typescript"], {multiEmptyLine: true});
