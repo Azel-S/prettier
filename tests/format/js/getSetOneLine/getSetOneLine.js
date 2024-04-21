eacb358f25b54debfd17b2cad602db1433a7810b
Sajani <sajju2315@gmail.com>
2024-04-20 20:37:42 -0400

    added explanations

--- tests/format/js/getSetOneLine/getSetOneLine.js
+++ tests/format/js/getSetOneLine/getSetOneLine.js
@@ -1,9 +1,11 @@
+//when true, line in getter is placed on same line as get()
 const objGet = {
     get name() {
       return this.name;
     },
   };
 
+//when true, remains unchanged (exceeds print width range)
 const objGetTwo = {
     get nameTwo() {
         this.name = this.name + " is valid";
@@ -11,12 +13,14 @@ const objGetTwo = {
     },
 }
 
+//when true, line in setter is placed on same line as set() 
 const objSet = {
     set name(name) {
       return this.name = name;
     },
 };
 
+//when true, remains unchanged (exceeds print width range)
 const objSetTwo = {
     set nameTwo(name) {
       return this.name + "may not be valid in this context because it is going out of range for print";
